6cd24534d08f2bedae51d83c583433eec3229d0c
Alena Grechka <alyonagrechka1@gmail.com>
2023-02-16 20:57:02 +0000

    step 1

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class MyHomePage extends HookWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final dateTime = useStream(getTime());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(dateTime.data ?? "Home Page"),
+      ),
+    );
+  }
+}
