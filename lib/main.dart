114724b6166f14fb3ac0132399777aa44452df28
Alena Grechka <alyonagrechka1@gmail.com>
2023-02-26 20:04:54 +0000

    step 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -30,26 +31,37 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const url = 'https://bit.ly/3KE6nqA';
+class CountDown extends ValueNotifier {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(
+      const Duration(seconds: 1),
+      (v) => from - v,
+    ).takeWhile((value) => value >= 0).listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then((data) => data.buffer.asUint8List())
-          .then((data) => Image.memory(data)),
-    );
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
       ),
-      body: Column(
-        children: [snapshot.data!].compactMap().toList(),
+      body: Text(
+        notifier.value.toString(),
       ),
     );
   }
