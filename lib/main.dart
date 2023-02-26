7bf06a19ba4d8e45a9fa9e28915f3ab25c889925
Alena Grechka <alyonagrechka1@gmail.com>
2023-02-26 15:34:49 +0000

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform,
+  ]) =>
+      map(
+        transform ?? (e) => e,
+      ).where((e) => e != null).cast();
+}
 
 void main() {
   runApp(const MyApp());
@@ -20,35 +30,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url = 'https://bit.ly/3KE6nqA';
+
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
+    final future = useMemoized(
+      () => NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((data) => data.buffer.asUint8List())
+          .then((data) => Image.memory(data)),
     );
+    final snapshot = useFuture(future);
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home Page"),
-        ),
-        body: Column(
-          children: [
-            TextField(
-              controller: controller,
-            ),
-            Container(
-              child: Text('Your value is ${text.value}'),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Column(
+        children: [snapshot.data!].compactMap().toList(),
+      ),
+    );
   }
 }
